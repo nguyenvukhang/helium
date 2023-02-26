43d10ecb4b4b1be3ff6e307415e8e2281785893c
nguyenvukhang <brew4k@gmail.com>
2023-02-26 09:24:45 +0800

    feat: add briding header

---
+++ menubar/WacomKit/WacomKit/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  WacomKit
+//
+//  Created by khang on 26/2/23.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "WacomTabletDriver.h"
+
+#endif /* Bridging_Header_h */
