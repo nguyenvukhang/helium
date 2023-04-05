3c6784d564918503060817d794e667a8a0378c23
nguyenvukhang <brew4k@gmail.com>
2023-04-05 10:32:51 +0800

    feat: listen for keystrokes

--- Helium/Helium/AppDelegate.swift
+++ Helium/Helium/AppDelegate.swift
@@ -9,6 +9,39 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var lastUsedTablet: Int
+
+    override init() {
+        self.lastUsedTablet = 0 // 0 is an invalid tablet ID
+        super.init()
+        self.listenForEvents()
+    }
+
+    /**
+     * Acts on a detected keypress.
+     *
+     * Current list of effective keypresses:
+     *   - Cmd + Shift + F2: start precision mode at cursor location
+     */
+    private func handleKeyDown(_ e: NSEvent) {
+        if e.modifierFlags.contains([.command, .shift]) && e.keyCode == KeyCode.f2.rawValue {
+            NSLog("[keypress] Command + Shift + F2")
+        }
+    }
+
+    /**
+     * This must only be called once.
+     */
+    private func listenForEvents() {
+        NSEvent.addGlobalMonitorForEvents(matching: [.tabletProximity, .keyDown]) { event in
+            if event.type == .tabletProximity && event.isEnteringProximity {
+                self.lastUsedTablet = event.systemTabletID
+            } else if event.type == .keyDown {
+                self.handleKeyDown(event)
+            }
+        }
+    }
+
     func applicationDidFinishLaunching(_: Notification) {
         // Insert code here to initialize your application
     }
@@ -18,6 +51,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationSupportsSecureRestorableState(_: NSApplication) -> Bool {
-        return true
+        true
     }
 }
