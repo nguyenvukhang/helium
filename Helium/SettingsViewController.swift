fff066829cbd3ad26bd070124471ac6d3654a04a
nguyenvukhang <brew4k@gmail.com>
2023-04-05 16:56:48 +0800

    feat: slider change modify value of text

--- Helium/SettingsViewController.swift
+++ Helium/SettingsViewController.swift
@@ -8,7 +8,8 @@
 import Cocoa
 
 class SettingsViewController: NSViewController {
-    @IBOutlet weak var butt: NSButtonCell!
+    @IBOutlet weak var scaleValue: NSTextFieldCell!
+    @IBOutlet weak var scaleSlider: NSSliderCell!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,11 @@ class SettingsViewController: NSViewController {
 
     @IBAction func pressed(_ sender: Any) {
         NSLog("GOTTEM HAHAHAHA")
+    }
+    @IBAction func scaleDidChange(_ sender: NSSliderCell) {
+        NSLog("Print")
+        NSLog("%0.4f",sender.doubleValue)
+        scaleValue.title = String(format: "%0.2f", sender.doubleValue)
     }
     
 }
