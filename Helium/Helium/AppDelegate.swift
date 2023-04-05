65f13221a747aeda3a2358f320eb7c50b34e6eca
nguyenvukhang <brew4k@gmail.com>
2023-04-05 09:42:18 +0800

    feat: start a swift project

---
+++ Helium/Helium/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Helium
+//
+//  Created by khang on 5/4/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_: NSApplication) -> Bool {
+        return true
+    }
+}
---
+++ Helium/Helium/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Helium
+//
+//  Created by khang on 5/4/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
