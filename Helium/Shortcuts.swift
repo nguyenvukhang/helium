24974519f2c123fc45b8629add5c320f51384a34
nguyenvukhang <brew4k@gmail.com>
2023-04-08 23:42:47 +0800

    feat: retrieve key equivalent of binding

--- Helium/Shortcuts.swift
+++ Helium/Shortcuts.swift
@@ -5,6 +5,7 @@
 //  Created by khang on 7/4/23.
 //
 
+import Cocoa
 import MASShortcut
 
 final class Shortcuts {
@@ -20,5 +21,10 @@ final class Shortcuts {
 
     static func associateView(_ view: MASShortcutView, toKey: Key) {
         view.associatedUserDefaultsKey = toKey.rawValue
+    }
+
+    static func getKeyEquivalent(_ key: Key) -> (String?, NSEvent.ModifierFlags)? {
+        guard let m = MASShortcutBinder.shared()?.value(forKey: key.rawValue) as? MASShortcut else { return nil }
+        return (m.keyCodeStringForKeyEquivalent, m.modifierFlags)
     }
 }
